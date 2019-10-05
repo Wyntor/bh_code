03565a7c7cb37fc6698b5e664488b4ab8b8148e2
王涛 <18840830318@163.com>
2019-10-05 19:59:12 +0800

    add2

---
+++ Bezier_frame/Bezier/curve.cpp
@@ -0,0 +1,63 @@
+
+#include "curve.h"
+#include <math.h>
+#include <glut.h>
+
+//计算n!/(k!(n-k)!),存储到c[k],其中k=0,1,...,n
+void BezierCurve::binomiaCoeffs(int n,float *c){
+
+	int k,i;
+
+	for(k=0;k<=n;k++){
+		//计算n!/(k!(n-k)!)
+		c[k]=1;
+		for(i=n;i>=k+1;i--)
+			c[k]*=i;
+		for(i=n-k;i>=2;i--)
+			c[k]/=i;
+	}
+
+}
+
+
+//Bezier曲线中,起点为B(0),终点为B(1),该函数计算点B(u)的坐标，其中0<=u<=1;
+//bezPt 指向B(u)的指针,
+//nCtrlPts 控制点个数
+//ctrlPts 控制点内存起始地址
+void BezierCurve::computeBezPt(float u,wcPt3D *bezPt,int nCtrlPts,wcPt3D *ctrlPts,float *C){
+
+	//powf(x,k) x的k次方
+	//添加代码
+	int k, n = nCtrlPts - 1;
+	float frac_sum;
+	bezPt->x = bezPt->y = bezPt->z = 0.0;
+	for (k = 0; k < nCtrlPts; k++) {
+		frac_sum = C[k] * pow(u, k)*pow(1 - u, n - k);
+		bezPt->x += ctrlPts[k].x * frac_sum;
+		bezPt->y += ctrlPts[k].y * frac_sum;
+		bezPt->z += ctrlPts[k].z * frac_sum;
+	}
+}
+
+
+//计算bezier曲线
+//ctrlPts 控制点数组内存起始地址
+//nCtrlPts 控制点个数
+//bezCurvePts 曲线点数组内存起始地址
+//nBezCurvePts 曲线点个数
+void BezierCurve::bezier(wcPt3D *ctrlPts,int nCtrlPts,wcPt3D *bezCurvePts,int nBezCurvePts){
+
+	//添加代码
+	float *C;
+	float u;
+	wcPt3D bezpt;
+	C = new float[nCtrlPts];
+	binomiaCoeffs(nCtrlPts - 1, C);
+	for (int i = 0; i < nBezCurvePts; i++) {
+		u = float(i) / float(nBezCurvePts);
+		computeBezPt(u, &bezpt, nCtrlPts, ctrlPts, C);
+		bezCurvePts[i].x = bezpt.x;
+		bezCurvePts[i].y = bezpt.y;
+		bezCurvePts[i].z = bezpt.z;
+	}
+}
---
+++ Bezier_frame/Bezier/line.cpp
@@ -0,0 +1,87 @@
+
+#include <math.h>
+#include "common.h"
+#include "line.h"
+
+//DDA算法绘制直线
+//(xa,ya),起点像素坐标
+//(xb,yb),终点像素坐标
+//setPixel,像素处理函数
+void DDA(int xa,int ya,int xb,int yb,void (*setPixel)(int x,int y)){
+
+	//添加代码ggg
+	int dx, dy, steps, k;
+	float delta_x, delta_y, x, y;
+	x = xa;
+	y = ya;
+	dx = xb - xa;
+	dy = yb - ya;
+	if (abs(dx) >= abs(dy)) {
+		steps = abs(dx);//斜率小于等于1
+	}
+	else {
+		steps = abs(dy);
+	}
+	delta_x = (float)dx / (float)steps;
+	delta_y = (float)dy / (float)steps;
+	for (k = 0; k <= steps; k++) {
+		setPixel(int(x + 0.5), int(y + 0.5));
+		x += delta_x;
+		y += delta_y;
+	}
+
+}
+
+//Bresenham算法绘制直线
+//参数同DDA
+void Bres(int xa,int ya,int xb,int yb,void (*setPixel)(int x,int y)){
+
+	bool u=false,v=false,w=false;
+	int tx,ty;
+	int dsx=xb-xa;
+	int dsy=yb-ya;
+
+	if(dsx<0){
+		dsx=-dsx;//dsx取反
+		u=true;
+	}
+	if(dsy<0){
+		dsy=-dsy;//dsy取反
+		v=true;
+	}
+	if(dsx<dsy){
+		swap(dsx,dsy);//dsx,dsy互换
+		w=true;
+	}
+
+	int dx=dsx,dy=dsy;
+	int p=2*dy-dx;
+	int twoDy=2*dy,twoDyDx=2*(dy-dx);
+	int x,y,xEnd;
+
+	x=0;
+	y=0;
+	xEnd=dsx;
+	setPixel(xa,ya);
+
+	while(x<xEnd){
+		x++;
+
+		if(p<0)
+			p+=twoDy;
+		else{
+			y++;
+			p+=twoDyDx;
+		}
+		tx=ROUND(x);
+		ty=ROUND(y);
+
+		if(w)swap(tx,ty);
+		if(v)ty=-ty;
+		if(u)tx=-tx;
+
+		tx+=xa;
+		ty+=ya;
+		setPixel(tx,ty);
+	}
+}
